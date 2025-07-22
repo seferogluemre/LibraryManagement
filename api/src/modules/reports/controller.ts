39df42c0901c344e9b653f79faacf0989086f55f
seferogluemre <yunusemre5331@icloud.com>
2025-07-22 12:00:24 +0300

    Refactor Reports Controller for Improved Readability
    - Reformatted the reports controller code for better readability and consistency.
    - Maintained existing functionality while enhancing the structure of the GET endpoints for reports, dashboard stats, and transfer stats.

--- api/src/modules/reports/controller.ts
+++ api/src/modules/reports/controller.ts
@@ -9,19 +9,29 @@ export const app = new Elysia({
   detail: {
     tags: ["Reports"],
   },
-}).get(
-  "",
-  async ({ query }) => {
-    const limit = query.limit || 5;
-    const reports = await ReportsService.getReports(limit as number);
-    return ReportsFormatter.response(reports);
-  },
-  reportIndexDto
-).get("/dashboard-stats", async () => {
-  const stats = await ReportsService.getSystemStats();
-  return stats;
-}, dashboardStatsDto)
-.get("/transfer-stats", async () => {
-  const stats = await ReportsService.getTransferStats();
-  return stats;
-}, transferStatsDto);
+})
+  .get(
+    "",
+    async ({ query }) => {
+      const limit = query.limit || 5;
+      const reports = await ReportsService.getReports(limit as number);
+      return ReportsFormatter.response(reports);
+    },
+    reportIndexDto
+  )
+  .get(
+    "/dashboard-stats",
+    async () => {
+      const stats = await ReportsService.getSystemStats();
+      return stats;
+    },
+    dashboardStatsDto
+  )
+  .get(
+    "/transfer-stats",
+    async () => {
+      const stats = await ReportsService.getTransferStats();
+      return stats;
+    },
+    transferStatsDto
+  );
